c645f9787b1ff29332e2d22af27babf67f472c7f
Janyu <54945627+Janyu427@users.noreply.github.com>
2025-03-10 15:15:00 +0800

    test deploy to vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/nuxt-tei/' : '/'
-  },
+  app: {},
 
   vite: {
     css: {
